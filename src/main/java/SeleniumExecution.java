4ae751882e407f68101f6cdd85a40824ac1df805
Francisco Moreno <francisco.moreno@sngular.com>
2020-06-15 15:52:07 +0200

    Firefox

--- src/main/java/SeleniumExecution.java
+++ src/main/java/SeleniumExecution.java
@@ -1,12 +1,22 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SeleniumExecution {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+        System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver.exe");
 
         WebDriver driver = new ChromeDriver();
+
+        //WebDriver driver = new FirefoxDriver();
+
+        //driver.manage().window().setSize(new Dimension(320,300));
+
+        //driver.manage().window().maximize();
+
 
         driver.get("https://duckduckgo.com/");
 
